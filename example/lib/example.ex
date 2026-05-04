cb6b2dc1217fd106c18beba738bbc7f177b46264
Leonty Chudinov <leonty.chudinov@gmail.com>
2026-05-04 16:53:34 +0500

    Strings and atoms

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,16 +1,26 @@
 defmodule Example do
   use Application
-
+  @y 10
   alias UUID
 
   def start(_type, _args) do
     IO.puts(UUID.uuid4())
     IO.puts(Example.hello())
+    Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
   def hello do
     IO.puts(:world)
+  end
+
+  def main do
+    x = 10
+    IO.puts("Example output")
+    IO.puts(x)
+    IO.puts(@y)
+    IO.puts(:hello)
+    IO.puts(:"hello world")
   end
 
 end
