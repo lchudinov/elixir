df15b653d56629e16f396eecf16728b03a6f005a
Leonty Chudinov <leonty.chudinov@gmail.com>
2026-05-08 19:42:19 +0500

    Add users page

--- forum/lib/forum_web/controllers/page_controller.ex
+++ forum/lib/forum_web/controllers/page_controller.ex
@@ -4,4 +4,13 @@ defmodule ForumWeb.PageController do
   def home(conn, _params) do
     render(conn, :home)
   end
+
+  def users(conn, _params) do
+    IO.puts("Users function hit!")
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Bob", email: "bob@email.com"}
+    ]
+    render(conn, :users, users: users)
+  end
 end
