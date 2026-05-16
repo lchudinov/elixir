a3dc423d5d508aebf600d930203d966420a03296
Leonty Chudinov <leonty.chudinov@gmail.com>
2026-05-16 10:32:14 +0500

    Add os_mon

--- forum/mix.exs
+++ forum/mix.exs
@@ -21,7 +21,7 @@ defmodule Forum.MixProject do
   def application do
     [
       mod: {Forum.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
