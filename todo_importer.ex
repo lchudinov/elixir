f2cddd1079c08680b449308040bcd433cd64f93e
Leonty Chudinov <leonty.chudinov@gmail.com>
2026-07-01 06:59:35 +0500

    Collectable

--- todo_importer.ex
+++ todo_importer.ex
@@ -54,5 +54,38 @@ defmodule TodoList.CsvImporter do
   end
 end
 
+defimpl String.Chars, for: TodoList do
+  def to_string(_) do
+    "#TodoList"
+  end
+end
+
+defimpl Collectable, for: TodoList do
+  def into(original) do
+    {original, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp into_callback(todo_list, :done) do
+    todo_list
+  end
+
+  defp into_callback(_todo_list, :halt) do
+    :ok
+  end
+end
+
 todo_list = TodoList.CsvImporter.import("todos.csv")
 IO.inspect(TodoList.entries(todo_list, ~D[2023-12-19]))
+IO.puts(todo_list)
+
+entries = [
+  %{date: ~D[2023-12-19], title: "Dentist"},
+  %{date: ~D[2023-12-20], title: "Shopping"},
+  %{date: ~D[2023-12-19], title: "Movies"}
+]
+
+IO.inspect(Enum.into(entries, TodoList.new()))
