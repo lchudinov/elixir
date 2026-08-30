d0644009ca7ea6c261f7eabda40b7045c3c685f5
Leonty Chudinov <leonty.chudinov@gmail.com>
2026-08-30 16:32:53 +0500

    Stateful database server

---
+++ stateful_database_server.ex
@@ -0,0 +1,58 @@
+defmodule DatabaseServer do
+
+  def start do
+    spawn(fn ->
+      connection = :rand.uniform(1000)
+      loop(connection)
+    end)
+  end
+
+  def run_async(server_pid, query_def) do
+    send(server_pid, {:run_query, self(), query_def})
+  end
+
+  def get_result do
+    receive do
+      {:query_result, result} -> result
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(connection) do
+    receive do
+      {:run_query, caller, query_def} ->
+        query_result = run_query(connection, query_def)
+        send(caller, {:query_result, query_result})
+    end
+    loop(connection)
+  end
+
+  defp run_query(connection, query_def) do
+    Process.sleep(2000)
+    "Connection: #{connection}: #{query_def} result"
+  end
+
+end
+
+server_pid = DatabaseServer.start()
+
+DatabaseServer.run_async(server_pid, "query 1")
+result = DatabaseServer.get_result()
+IO.inspect(result, label: "query 1 result")
+
+DatabaseServer.run_async(server_pid, "query 2")
+result = DatabaseServer.get_result()
+IO.inspect(result, label: "query 2 result")
+
+pool = Enum.map(1..100, fn _ -> DatabaseServer.start() end)
+Enum.each(
+  1..5,
+  fn query_def ->
+    server_pid = Enum.at(pool, :rand.uniform(100) - 1)
+    DatabaseServer.run_async(server_pid, query_def)
+  end
+)
+
+results = Enum.map(1..5, fn _ -> DatabaseServer.get_result() end)
+IO.inspect(results, label: "results")
